c51c2ad404120f8a3cc977ee0e2be2e67369106d
Sam866 <aliganfoud@hotmail.fr>
2018-03-29 08:53:47 +0100

    Operational Game

--- js/app.js
+++ js/app.js
@@ -1,55 +1,168 @@
-/*
- * Create a list that holds all of your cards and clone it
- */
+// VARIABLES TO BE USED ACCROSS THE GAME
 
- var ul = document.querySelector(".deck"), // get the list
-     temp = ul.cloneNode(true); // clone the list
+let symbols = ['anchor', 'anchor', 'bicycle', 'bicycle', 'bolt', 'bolt', 'bomb', 'bomb', 'cube', 'cube', 'diamond', 'diamond', 'leaf', 'leaf', 'paper-plane-o', 'paper-plane-o'];
+let	openedCards = [];
+let	matchingCards = 0;
+let	movesCount = 0;
+let	delay = 400;
+let	chronometer;
+let	second = 0;
+let	$timer = $('.timer');
+let	totalCard = symbols.length / 2;
 
- // shuffle the cloned list
- for (var i = ul.children.length + 1; i--; )
-    ul.appendChild( ul.children[Math.random() * i |0] );
-    ul.shuffle().parentNode.replaceChild(ul);
+//FUNCTIONS TO BE USED ACCROSS THE GAME
 
- /*var ul = document.querySelector(".deck"), // get the list
-     temp = ul.cloneNode(true); // clone the list
-
- // shuffle the cloned list
- for (var i = temp.children.length + 1; i--; )
-     temp.appendChild( temp.children[Math.random() * i |0] );
- ul.shuffle().parentNode.replaceChild(temp, ul);*/
-
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
+// Shuffling method already provided https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+// && https://github.com/coolaj86/knuth-shuffle
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+	return array;
 }
 
+//Manage time with Window setInterval() Method
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+function startChronometer() {
+		chronometer = setInterval(function () {
+		second++
+		$timer.text(`${second}`)
+	}, 1000);
+}
+
+function resetChronometer($timer) {
+	if ($timer) {
+		clearInterval($timer);
+	}
+}
+
+// GAME ELEMENTS
+
+// Game starter and restarter
+function lauchGame() {
+	let cards = shuffle(symbols);
+	$('.deck').empty();
+	matchingCards = 0;
+	movesCount = 0;
+	$('.moves').text('0');
+	$('section ul li').hide();
+	$('section ul').append('<li><i class="fa fa-star"></i></li>');
+	$('section ul').append('<li><i class="fa fa-star"></i></li>');
+	$('section ul').append('<li><i class="fa fa-star"></i></li>');
+	for (let i = 0; i < cards.length; i++) {$('.deck').append($('<li class="card"><i class="fa fa-' + cards[i] + '"></i></li>'))}
+	cardControl();
+	resetChronometer(chronometer);
+	second = 0;
+	$timer.text(`${second}`)
+	startChronometer();
+};
+
+// Score setting
+function setRating(movesCount) {
+	let rating = 3;
+	if (movesCount > 10 && movesCount <= 16) {
+		rating = 2;
+	} else if (movesCount > 16 && movesCount <= 20) {
+		$('section ul li').hide();
+		$('section ul').append('<li><i class="fa fa-star"></i></li>');
+		$('section ul').append('<li><i class="fa fa-star"></i></li>');
+		rating = 1;
+	} else if (movesCount > 20) {
+		$('section ul li').hide();
+		rating = 0;
+	}
+	return { score: rating };
+};
+
+
+// End Game
+function endGame(movesCount, score) {
+	swal({
+		allowEscapeKey: false,
+		allowOutsideClick: false,
+		title: 'Congratulations! You Made It!',
+		text: 'In ' + movesCount + ' Moves and ' + score + ' Stars in ' + second + ' Seconds!',
+		type: 'success',
+		confirmButtonColor: '#EE82EE',
+		confirmButtonText: 'Play again!'
+	}).then(function (isConfirm) {
+		if (isConfirm) {
+			lauchGame();
+		}
+	})
+}
+
+// Restart Game
+$('.restart').on('click', function () {
+	swal({
+		allowEscapeKey: false,
+		allowOutsideClick: false,
+		title: "Are you sure?",
+		text: "Your progress will be Lost!",
+		type: "warning",
+		showCancelButton: true,
+		confirmButtonColor: '#02ccba',
+		cancelButtonColor: '#f95c3c',
+		confirmButtonText: "Yes, Restart Game!"
+	}).then(function (isConfirm) {
+		if (isConfirm) {
+			lauchGame();
+		}
+	})
+});
+
+
+// GAME HEART
+var cardControl = function () {
+
+	// Card flip
+	$('.deck').find('.card').on('click', function () {
+
+		if ($(this).hasClass('show') || $(this).hasClass('match')) { return true; }
+
+		var card = $(this).context.innerHTML;
+
+		$(this).addClass('open show');
+		openedCards.push(card);
+
+		// Compare with opened card
+		if (openedCards.length > 1) {
+			if (card === openedCards[0]) {
+				$('.deck').find('.open').addClass('match animated infinite rubberBand');
+				setTimeout(function () {
+					$('.deck').find('.match').removeClass('open show animated infinite rubberBand');
+				}, delay);
+				matchingCards++;
+			} else {
+				$('.deck').find('.open').addClass('notmatch animated infinite wobble');
+				setTimeout(function () {
+					$('.deck').find('.open').removeClass('animated infinite wobble');
+				}, delay / 1.5);
+				setTimeout(function () {
+					$('.deck').find('.open').removeClass('open show notmatch animated infinite wobble');
+				}, delay);
+			}
+			openedCards = [];
+			movesCount++;
+			setRating(movesCount);
+			$('.moves').html(movesCount);
+		}
+
+		// End Game if match all cards
+		if (totalCard === matchingCards) {
+			setRating(movesCount);
+			var score = setRating(movesCount).score;
+			setTimeout(function () {
+				endGame(movesCount, score);
+			}, 500);
+		}
+	});
+};
+
+lauchGame();
